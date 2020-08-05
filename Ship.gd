cff8a78924cc5c884be200a766671dc4b528a702
Matthew Fyfe <mlf562@usask.ca>
2020-08-04 18:22:32 -0600

    Updated wind behaviour and base collision handling

--- Ship.gd
+++ Ship.gd
@@ -1,21 +1,37 @@
 extends KinematicBody
 
-var move_speed = 5
+var move_speed = 10
 var target_position = transform.origin
+var current_position = transform.origin
 
+#On object ready
 func _ready():
 	add_to_group("ships")
 	
+#Every physics tick
 func _physics_process(delta):
-	move_and_slide(target_position - transform.origin)
-	#move_and_collide(target_position - transform.origin)
+	#Called multiple times to speed up distance per tick
+	move_and_slide(target_position - transform.origin, Vector3(0, 0, 0), false, 1, 0.2617994, true)
+	move_and_slide(target_position - transform.origin, Vector3(0, 0, 0), false, 1, 0.2617994, true)
+	
+	#Handle collisions if needed
+	if get_slide_count() > 0:
+		#Check what we hit
+		print(get_slide_collision(0).collider.name)
+		#Go back to last grid location
+		target_position = current_position
 
+#Handle wind blowing ships
 func blow_direction(direction):
+	#Update our current (last) position and round off
+	current_position = transform.origin
+	#current_position.x = floor(current_position.x / 5) * 5
+	
 	if direction == "north":
-		target_position -= Vector3(0,0,-5)
+		target_position -= Vector3(0,0,-move_speed)
 	if direction == "south":
-		target_position -= Vector3(0,0,5)
+		target_position -= Vector3(0,0,move_speed)
 	if direction == "west":
-		target_position -= Vector3(-5,0,0)
+		target_position -= Vector3(-move_speed,0,0)
 	if direction == "east":
-		target_position -= Vector3(5,0,0)
+		target_position -= Vector3(move_speed,0,0)
